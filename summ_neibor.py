d8138f3eda1178c5c223b07fe9c08d7ea71453d1
Vladimir Merk <merkvladimir@gmail.com>
2017-12-04 09:29:23 +0300

    Изменение файла

--- summ_neibor.py
+++ summ_neibor.py
@@ -8,14 +8,6 @@ if not sys.argv[1]:
         a = str(input('Введите строку'))
 else:
     a = sys.argv[1]
-    print(a)
     with open(sys.argv[1], 'r') as file:
         a = file.readlines()
 
-
-
-
-# for x in range(len(lst)):
-#     for y in range(len(lst[x])):
-#         print(lst[x][y], end=' ')
-#     print()
